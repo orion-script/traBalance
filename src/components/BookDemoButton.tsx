06f619da756d113f789404ebecc4bfdc51455f31
Hadestech01 <jaolaniyi96@student.lautech.edu.ng>
2023-07-05 09:34:17 +0100

    add book demo button

---
+++ src/components/BookDemoButton.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+const BookDemoButton = () => {
+    return ( 
+        
+        <Link href="https://portal.trabalance.com/register" className="bg-[#4169e1] rounded-lg px-5 py-3 text-[#fff]">Book a demo</Link>
+     );
+}
+ 
+export default BookDemoButton;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,17 @@
 import Image from 'next/image'
 import HeroImage from "../assets/here.jpg"
+import BookDemoButton from './BookDemoButton';
 
 const Hero = () => {
     return ( 
         <section className="w-10/12 m-auto mt-20 flex flex-col md:flex-row justify-between">
             <div className="w-full md:w-2/5">
                 <p className="font-extrabold text-3xl md:text-5xl mb-5 shrink">Be on the samepage with everyone.</p>
-                <p className='text-[#6b7280]'>Send and receive documents, manage receivables and payables, and collaborate with your customers and vendors in one portal.</p>
+                <p className='text-[#6b7280] mb-6'>Send and receive documents, manage receivables and payables, and collaborate with your customers and vendors in one portal.</p>
+
+                <BookDemoButton />
             </div>
-            <Image src={HeroImage} alt="hero image" className='w-full md:w-2/5 mt-10 md:mt-0' />
+            <Image src={HeroImage} alt="hero image" className='w-full md:w-2/5 mt-16 md:mt-0' />
         </section>
      );
 }
